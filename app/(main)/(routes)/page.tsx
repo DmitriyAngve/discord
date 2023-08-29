18a0332561ea218f92e8c588de580a92cb365249
DmitriyAngve <kirirch129@gmail.com>
2023-08-29 15:22:17 +0700

    #

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,4 +1,5 @@
+import { UserButton } from "@clerk/nextjs/app-beta";
+
 export default function Home() {
-  return <div>This is a protected route.</div>;
+  return <UserButton afterSignOutUrl="/" />;
 }
-//  <Button className={cn("bg-indigo-500", state && "bg-red-500")}>Click me</Button> - if state is true, then bg-red-500
