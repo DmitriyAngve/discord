fbfd27a690d8d2d3a6960e3161d1613a79e1f0e9
DmitriyAngve <kirirch129@gmail.com>
2023-08-29 13:48:45 +0700

    #

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <p className="text-3xl font-bold text-indigo-500">Hello Discord Clone!</p>
+    <div className="">
+      <p className="text-3xl font-bold text-indigo-500">Hello Discord Clone!</p>
+      <Button>Click me</Button>
+    </div>
   );
 }
+//  <Button className={cn("bg-indigo-500", state && "bg-red-500")}>Click me</Button> - if state is true, then bg-red-500
