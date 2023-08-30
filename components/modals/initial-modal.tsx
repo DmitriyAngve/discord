5aae634219d71ca0e952f2a02b996bffcb48ff22
DmitriyAngve <kirirch129@gmail.com>
2023-08-30 14:07:40 +0700

    #

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -12,7 +12,17 @@ import {
 export const InitialModal = () => {
   return (
     <Dialog open>
-      <DialogContent className="bg-white text-black p-0 overflow-hidden"></DialogContent>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Customize your server
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Give your server a personality with a name and an image. You can
+            always change it later.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
     </Dialog>
   );
 };
