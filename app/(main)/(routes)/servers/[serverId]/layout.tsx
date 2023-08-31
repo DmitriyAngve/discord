18dcfdc1797309da21a685534098cab2438cd440
DmitriyAngve <kirirch129@gmail.com>
2023-08-31 16:38:10 +0700

    #

--- app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -1,14 +1,21 @@
+import { ServerSidebar } from "@/components/server/server-sidebar";
+
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
+
 import { redirectToSignIn } from "@clerk/nextjs";
+
+import { redirect } from "next/navigation";
 
 const ServerIdLayout = async ({
   children,
   params,
 }: {
   children: React.ReactNode;
+  params: { serverId: string };
 }) => {
   const profile = await currentProfile();
+
   if (!profile) {
     return redirectToSignIn();
   }
@@ -17,10 +24,26 @@ const ServerIdLayout = async ({
   const server = await db.server.findUnique({
     where: {
       id: params.serverId, // serverId - from prop "params" because I named folder "[serverId]"
+      members: {
+        some: {
+          profileId: profile.id, // загружаем только для члена, который загрузил этот сервер
+        },
+      },
     },
   });
 
-  return <div>{children}</div>;
+  if (!server) {
+    return redirect("/");
+  }
+
+  return (
+    <div className="h-full">
+      <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
+        <ServerSidebar serverId={params.serverId} />
+      </div>
+      <main className="h-full md:pl-60">{children}</main>
+    </div>
+  );
 };
 
 export default ServerIdLayout;
