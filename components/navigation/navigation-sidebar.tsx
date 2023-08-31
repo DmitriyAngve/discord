2e0b7f8965b819908acbd9b047874b195d3a4d77
DmitriyAngve <kirirch129@gmail.com>
2023-08-31 14:13:21 +0700

    #

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -2,10 +2,15 @@ import { redirect } from "next/navigation";
 
 import { Separator } from "@/components/ui/separator";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { currentProfile } from "@/lib/current-profile";
+import { ModeToggle } from "@/components/mode-toggle";
+
 import { db } from "@/lib/db";
+import { currentProfile } from "@/lib/current-profile";
+
 import { NavigationAction } from "./navigation-action";
 import { NavigationItem } from "./navigaion-item";
+
+import { UserButton } from "@clerk/nextjs";
 
 export const NavigationSidebar = async () => {
   const profile = await currentProfile();
@@ -39,6 +44,17 @@ export const NavigationSidebar = async () => {
           </div>
         ))}
       </ScrollArea>
+      <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+        <ModeToggle />
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              avatarBox: "h-[48px] w-[48px]",
+            },
+          }}
+        />
+      </div>
     </div>
   );
 };
