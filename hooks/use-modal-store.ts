5856af36e6bb80a3482b2cc48e175ab550140990
DmitriyAngve <kirirch129@gmail.com>
2023-09-01 12:42:45 +0700

    #

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,20 +1,28 @@
-// хук для контролирования всеъ модулей в зустанд
+// хук для контролирования всех модулей в зустанд
+
+import { Server } from "@prisma/client";
 import { create } from "zustand";
 
-export type ModalType = "createServer"; // определяет пользовательский тип "ModalType", который имеет одно значение "createServer" - это ограниченный набор возможных типов модальных окон
+export type ModalType = "createServer" | "invite";
+// определяет пользовательский тип "ModalType", который имеет одно значение "createServer" - это ограниченный набор возможных типов модальных окон
+interface ModalData {
+  server?: Server;
+}
 
 interface ModalStore {
   type: ModalType | null;
+  data: ModalData;
   isOpen: boolean;
-  onOpen: (type: ModalType) => void;
+  onOpen: (type: ModalType, data?: ModalData) => void;
   onClose: () => void;
 } // описывает состояние и действия, доступные в этом хранилище состояния
 // isOpen :(type: ModalType) => void - метод "onOpen" используетя для открытия модального окна с заданным типом. Он принимает аргумент "type" и устанавливает соответсвующий тип модального окна в св-во "type"
 
 export const useModal = create<ModalStore>((set) => ({
   type: null,
+  data: {},
   isOpen: false,
-  onOpen: (type) => set({ isOpen: true, type }),
+  onOpen: (type, data = {}) => set({ isOpen: true, type, data }),
   onClose: () => set({ type: null, isOpen: false }),
 }));
 // "export const useModal = create<ModalStore>((set) => ({})" - эта строка создает хранилище Zustand, используя функцию "create". Внутри функции "create" определенно начальное состояние модального окна, где "type" равно "null" и тд
