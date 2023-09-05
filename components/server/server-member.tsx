44270ce1b65a7483611c9f4c1eb2a0c841e9a07e
DmitriyAngve <kirirch129@gmail.com>
2023-09-05 16:32:43 +0700

    #

--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -1,5 +1,12 @@
 "use client";
 
-export const ServerMember = () => {
+import { Member, Profile, Server } from "@prisma/client";
+
+interface ServerMemberProps {
+  member: Member & { profile: Profile };
+  server: Server;
+}
+
+export const ServerMember = ({ member, server }: ServerMemberProps) => {
   return <div>Server Member</div>;
 };
