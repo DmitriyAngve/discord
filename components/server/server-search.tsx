a37a8bc0c964aa0c9d01f34b0ce2140213e2a7b9
DmitriyAngve <kirirch129@gmail.com>
2023-09-05 13:38:17 +0700

    #

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Search } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   CommandDialog,
   CommandEmpty,
@@ -10,6 +10,7 @@ import {
   CommandItem,
   CommandList,
 } from "@/components/ui/command";
+import { useParams, useRouter } from "next/navigation";
 
 interface ServerSearchProps {
   data: {
@@ -27,6 +28,40 @@ interface ServerSearchProps {
 
 export const ServerSearch = ({ data }: ServerSearchProps) => {
   const [open, setOpen] = useState(false);
+  const router = useRouter();
+  const params = useParams();
+
+  const onClick = ({
+    id,
+    type,
+  }: {
+    id: string;
+    type: "channel" | "member";
+  }) => {
+    setOpen(false);
+
+    if (type === "member") {
+      return router.push(`/servers/${params?.serverId}/conversations/${id}`); // переход на новую страницу (переключает на разговор с участником)
+    }
+
+    if (type === "channel") {
+      return router.push(`/servers/${params?.serverId}/channels/${id}`);
+    } // переход на страницу канала
+  };
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      // функция происходит, когда нажимают на клавиатуру. Она получает объект "e"
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        // это условие, которое проверяет, нажата ли клавиша "k" одновременно с клавишей "ctrl" - если оба этих условия true => выполняется код внутри
+        e.preventDefault();
+        setOpen((open) => !open); // если "open" равно "true", то оно меняется на "false", и наоборот.
+      }
+    };
+    document.addEventListener("keydown", down); // добавляем слушателя события на всю страницу. Когда происходит нажатие клавиши, "down" функция будет вызвана
+    return () => document.removeEventListener("keydown", down);
+  }, []); // размонтируем, удаляем из DOM
+
   return (
     <>
       <button
@@ -34,12 +69,11 @@ export const ServerSearch = ({ data }: ServerSearchProps) => {
         className="group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition"
       >
         <Search className="w-4 h-4 text-zinc-500 dark:text-zinc-400" />
-        <p className="font-semibold text-sm text-zinc-500 dark:zinc-400 group-hover:text-zinc-600 dark:group-hover:text-zinc-300 transition">
+        <p className="font-semibold text-sm text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-600 dark:group-hover:text-zinc-300 transition">
           Search
         </p>
-        {/* kbd - keyboard */}
         <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground ml-auto">
-          <span className="text-xs">⌘</span>
+          <span className="text-xs">⌘</span>K
         </kbd>
       </button>
       <CommandDialog open={open} onOpenChange={setOpen}>
@@ -53,7 +87,10 @@ export const ServerSearch = ({ data }: ServerSearchProps) => {
               <CommandGroup key={label} heading={label}>
                 {data?.map(({ id, icon, name }) => {
                   return (
-                    <CommandItem key={id}>
+                    <CommandItem
+                      key={id}
+                      onSelect={() => onClick({ id, type })}
+                    >
                       {icon}
                       <span>{name}</span>
                     </CommandItem>
