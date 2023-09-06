6be2609f3a10981a8fb907189a4fac5cb53f1971
DmitriyAngve <kirirch129@gmail.com>
2023-09-06 15:43:48 +0700

    #

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,47 @@
-const ChannelIdPage = () => {
-  return <div>Channel ID Page!</div>;
+import { ChatHeader } from "@/components/chat/chat-header";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string; // смотри на структуру папок ([serverId] и [channelId] - обе внутри моего URL)
+  };
+}
+
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
+  // let's fetch the current Profile
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    },
+  });
+
+  if (!channel || !member) {
+    redirect("/");
+  }
+
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader />
+    </div>
+  );
 };
 
 export default ChannelIdPage;
---
+++ components/chat/chat-header.tsx
@@ -0,0 +1,7 @@
+import { Hash } from "lucide-react";
+
+interface ChatHeaderProps {}
+
+export const ChatHeader = () => {
+  return <div>Chat Header!</div>;
+};
